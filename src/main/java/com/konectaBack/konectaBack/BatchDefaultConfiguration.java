54979345bebbfb25064e40f751e51dbf1ff9ca82
“samuelgil007” <sagilar15@gmail.com>
2021-09-06 13:11:17 -0500

    Añadiendo batch v3, ahora se ejecuta con tareas automaticas

--- src/main/java/com/konectaBack/konectaBack/BatchDefaultConfiguration.java
+++ src/main/java/com/konectaBack/konectaBack/BatchDefaultConfiguration.java
@@ -2,8 +2,15 @@ package com.konectaBack.konectaBack;
 
 import org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.batch.JobLauncherCommandLineRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
 
 import javax.sql.DataSource;
 
--- src/main/java/com/konectaBack/konectaBack/KonectaBackApplication.java
+++ src/main/java/com/konectaBack/konectaBack/KonectaBackApplication.java
@@ -4,11 +4,13 @@ import com.konectaBack.konectaBack.Configuration.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @Import(SwaggerConfiguration.class)
+@EnableScheduling
 public class KonectaBackApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
